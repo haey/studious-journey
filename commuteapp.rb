624125b9068a2ef89fe498c5db02213cff28c32e
Haley Perkins <haley@stripe.com>
2017-06-25 19:34:45 -0700

    Futz with wording on Giants note

--- commuteapp.rb
+++ commuteapp.rb
@@ -3,7 +3,7 @@ require_relative "sf_home_game_parser"
 
 get '/' do
   if is_game_today?
-    @giants_status = "There's a Giants home game today! Abandon all commute hope."
+    @giants_status = "There's a Giants home game today! Your commute will likely be affected."
   else
     @giants_status = "No Giants home game today! Your commute may face other dangers."
   end
